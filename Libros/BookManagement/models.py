6bba4a983c1ca196e2236584e2009dc1c8f8ee83
Juan Chalita <chalajr2@gmail.com>
2022-02-16 14:06:25 -0600

    Co-authored-by: deuzleor <deuzleor@users.noreply.github.com>

--- Libros/BookManagement/models.py
+++ Libros/BookManagement/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+#Arturo
+class Autor(models.Model):
+    autor = models.CharField("Autor", max_length=100)
+    archivo = models.FileField(upload_to='uploads/files/archivos')
+    def __str__(self):
+        return self.autor
+
+#Dennis
+class Editorial(models.Model):
+        nombre = models.CharField("Nombre",max_length=50)
+        imagen = models.ImageField(upload_to='uploads/editoriales/imagenes')
+
+        def __str__(self):
+            return self.nombre
+
 class Book(models.Model):
     title = models.CharField("Titulo",max_length=50)
     description = models.TextField("Descripcion",max_length=500)
-    autor = models.CharField("Autor",max_length=50)
+    # autor = models.ForeignKey(Autor, on_delete=CASCADE)
+    # editorial = models.ForeignKey(Editorial, on_delete=CASCADE)
 
     def __str__(self):
         return self.title
--- Libros/BookManagement/views.py
+++ Libros/BookManagement/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import BookForm
+from .models import *
 # Create your views here.
 
 
 def bookCreate(request):
-    bookForm = BookForm()
-    return render(request, "BookManagement/bookCreate.html", {
-        "bookForm": bookForm
-    })
+    #Revisamos el metodo de entrada
+    if request.method == "POST":
+        #Creamos una variable con el formulario y la informacion que envio el usuario
+        bookFormRequest = BookForm(request.POST)
+        #Validamos el formulario
+        if bookFormRequest.is_valid():
+            #Guardamos la nueva entidad en la base de datos
+            bookFormRequest.save()
+        #Redireccionamos al usuario
+        return redirect('BookManagement:bookCreate')
+    else:
+        bookForm = BookForm()
+        bookList = Book.objects.all()
+        return render(request, "BookManagement/bookCreate.html", {
+            "bookForm": bookForm,
+            "bookList": bookList
+        })
 
 def bookRead(request):
     return
@@ -18,3 +32,5 @@ def bookUpdate(request):
 
 def bookDelete(request):
     return
+
+    
